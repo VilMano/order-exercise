cd9ea76e522170b3a3ba55a997a792edf371665e
VilMano <mano.gsat@hotmail.com>
2021-06-29 21:11:35 +0100

    added update and delete to product

--- MODEL/product.php
+++ MODEL/product.php
@@ -29,13 +29,46 @@
         }
 
         function create($product){
-            $sql = 'INSERT INTO '. $this->table_name .' (name, price) VALUES (:name, :price);';
-            $pQuery = $this->conn->prepare($sql);
-            $pQuery->bindParam(':name', $product->name);
-            $pQuery->bindParam('price', $product->price);
-            $pQuery->execute();
+            try{
+                $sql = 'INSERT INTO '. $this->table_name .' (name, price) VALUES (:name, :price);';
+                $pQuery = $this->conn->prepare($sql);
+                $pQuery->bindParam(':name', $product->name);
+                $pQuery->bindParam('price', $product->price);
+                $pQuery->execute();
+    
+                return $this->conn->lastInsertId();
+            }catch(PDOException $e){
+                throw new Exception($e->getMessage());
+            }
+        }
 
-            return $this->conn->lastInsertId();
+        function update($product){
+            try{
+                $sql = 'UPDATE '. $this->table_name .' SET name = :name, price = :price WHERE id = :id;';
+                $pQuery = $this->conn->prepare($sql);
+                $pQuery->bindParam(':name', $product->name);
+                $pQuery->bindParam(':price', $product->price);
+                $pQuery->bindParam(':id', $product->id);
+
+                $pQuery->execute();
+                return $product->id;
+            }catch(PDOException $e){
+                throw new Exception($e->getMessage());
+            }
+        }
+
+        function delete($id){
+            try{
+                $sql = 'DELETE FROM '. $this->table_name .' WHERE id = :id;';
+                $pQuery = $this->conn->prepare($sql);
+                $pQuery->bindParam(':id', $id);
+    
+                $pQuery->execute();
+                return $id;
+
+            }catch(PDOException $e){
+                throw new Exception($e->getMessage());
+            }
         }
     }
 ?>
---
+++ api/product/update.php
@@ -0,0 +1,18 @@
+<?php 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    header("Content-Type: application/json;");
+    include_once '../../DAL/database.php';
+    include_once '../../MODEL/product.php';
+
+    $database = new Database();
+    $conn = $database->getConnection();
+
+    $productObj = new Product($conn);
+
+    $param = file_get_contents('php://input');
+    $product = json_decode($param);
+    echo $productObj->update($product);
+?>
